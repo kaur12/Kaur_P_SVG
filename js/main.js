13cd3fa3397c1347f0e520d82b7a9a3aad6bf4ed
Kaur <p_kaur92032@tss-hr420-dm19.fc.ca>
2019-01-14 16:18:01 -0500

    adding initial script

---
+++ js/main.js
@@ -0,0 +1,12 @@
+(() => {
+	//self-invoking anonymous function(or Lambda)
+	//
+	//var=> maens create a variable
+	//whatever comes after the = is what the varible ref
+	var images = document.querySelectorAll('img');
+
+	//document is always the webpage
+	//after the dot is always a method or a property
+
+	console.log(images);
+})();
